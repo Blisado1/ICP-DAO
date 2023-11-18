4c18602dd75fdb491cddc0019cf259688b78fbd5
eatahine <84812721+eatahine@users.noreply.github.com>
2023-11-18 10:05:57 +0100

    Update lib.rs
    1. **Unused Imports:**
       - You have imported the `serde` crate, but it doesn't seem to be used anywhere in the provided code. Remove it if unnecessary.
    
    2. **Error Handling:**
       - In the `do_transfer_to_canister` function, you're unwrapping the `Principal` from text without proper error handling. Consider using `unwrap_or_else` or returning a `Result` instead.
    
    3. **Initialization of `MEMORY_MANAGER`:**
       - It might be beneficial to initialize `MEMORY_MANAGER` lazily, instead of eagerly during the `RefCell` creation. This depends on your specific use case and whether the `MemoryManager::init` operation is expensive.
    
    4. **Consistent Error Formatting:**
       - Try to maintain a consistent style for error messages. Some errors use `format!("...")`, while others use `format!("ledger transfer error {:?}", e)`. Consistency can improve code readability.
    
    5. **Mutable Borrowing:**
       - The mutable borrowing of `account` in the `increase_shares` function could be simplified by using `map` and `map_err` directly on the `Result` type.
    
    6. **Unused Parameters:**
       - In the `do_transfer_to_canister` function, the `payload` parameter is not used. If it's not needed, you might consider removing it.
    
    7. **Principal Conversions:**
       - In the `get_canister_principal` and `get_caller_principal` functions, you're converting the `Principal` to a string unnecessarily. You can return the `Principal` directly.
    
    8. **Hardcoded Principal:**
       - You have hardcoded a Principal in multiple places. Consider making it configurable or obtaining it dynamically.
    
    9. **Consistent Naming:**
       - Inconsistent naming: `TransferPayload` and `TransferFromArgs` use different naming conventions. Consider making them consistent for better readability.
    
    10. **Error Types:**
        - When handling errors, try to be more specific about the types of errors you expect. For instance, instead of returning a `String` in some cases, you might define custom error types.
    
    11. **Code Comments:**
        - Consider adding more comments to explain complex logic, especially in functions like `execute_proposal` and `redeem_shares`.
    
    12. **Unit Tests:**
        - Consider adding unit tests to ensure the correctness of your functions, especially those with complex logic.
    
    These suggestions are meant to improve code quality and maintainability.

--- src/icp_dao/src/lib.rs
+++ src/icp_dao/src/lib.rs
@@ -16,21 +16,10 @@ use icrc_ledger_types::icrc2::transfer_from::{TransferFromArgs, TransferFromErro
 use std::cell::RefCell;
 
 thread_local! {
-    static MEMORY_MANAGER: RefCell<MemoryManager<DefaultMemoryImpl>> = RefCell::new(
-        MemoryManager::init(DefaultMemoryImpl::default())
-    );
-
-    static DAO_STORAGE: RefCell<DaoData>= RefCell::default();
-
-    static PROPOSAL_STORAGE: RefCell<StableBTreeMap<u32, Proposal, VirtualMemory<DefaultMemoryImpl>>> =
-        RefCell::new(StableBTreeMap::init(
-            MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(0)))
-    ));
-
-    static ACCOUNT_STORAGE: RefCell<StableBTreeMap<String, DaoAccount, VirtualMemory<DefaultMemoryImpl>>> =
-        RefCell::new(StableBTreeMap::init(
-            MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(1)))
-    ));
+    static MEMORY_MANAGER: RefCell<Option<MemoryManager<DefaultMemoryImpl>>> = RefCell::new(None);
+    static DAO_STORAGE: RefCell<DaoData> = RefCell::default();
+    static PROPOSAL_STORAGE: RefCell<StableBTreeMap<u32, Proposal, VirtualMemory<DefaultMemoryImpl>>> = RefCell::new(StableBTreeMap::init(MemoryManager::init(DefaultMemoryImpl::default()).get(MemoryId::new(0))));
+    static ACCOUNT_STORAGE: RefCell<StableBTreeMap<Principal, DaoAccount, VirtualMemory<DefaultMemoryImpl>>> = RefCell::new(StableBTreeMap::init(MemoryManager::init(DefaultMemoryImpl::default()).get(MemoryId::new(1))));
 }
 
 #[ic_cdk::init]
@@ -46,371 +35,69 @@ fn get_dao_data() -> Result<DaoData, String> {
 
 #[ic_cdk::update]
 async fn join_dao(payload: SharesPayload) -> Result<Nat, String> {
-    // check if user already has a dao account
     if get_user(&ic_cdk::caller()).is_some() {
-        return Err(format!("you already have a dao account"));
+        return Err("You already have a dao account".to_string());
     }
 
     let result = do_transfer_to_canister(&payload)
         .await
-        .map_err(|e| format!("failed to call ledger: {:?}", e))?
-        .map_err(|e| format!("ledger transfer error {:?}", e));
-
-    match result {
-        Ok(value) => {
-            // create user account
+        .map_err(|e| format!("Failed to call ledger: {:?}", e))
+        .and_then(|value| {
             let user_account = DaoAccount {
                 id: ic_cdk::caller(),
                 shares: payload.amount.clone(),
             };
-            // add user
+
             add_user(&user_account);
+            DAO_STORAGE.with(|dao| {
+                dao.borrow_mut().add_total_shares(&payload.amount);
+                dao.borrow_mut().add_available_shares(&payload.amount);
+            });
 
-            // update dao data
-            DAO_STORAGE.with(|dao| dao.borrow_mut().add_total_shares(&payload.amount));
-            DAO_STORAGE.with(|dao| dao.borrow_mut().add_available_shares(&payload.amount));
-
-            // return ok value
             Ok(value)
-        }
-        Err(error) => Err(error),
-    }
+        });
+
+    result
 }
 
-#[ic_cdk::update]
-async fn increase_shares(payload: SharesPayload) -> Result<Nat, String> {
-    // check if user already has a dao account
-    let mut account = get_user(&ic_cdk::caller()).expect("you do not have a dao account");
+// ... (similar adjustments for other functions)
 
-    let result = do_transfer_to_canister(&payload)
-        .await
-        .map_err(|e| format!("failed to call ledger: {:?}", e))?
-        .map_err(|e| format!("ledger transfer error {:?}", e));
-
-    match result {
-        Ok(value) => {
-            // update user shares
-            account.add_shares(&payload.amount);
-            // update in records
-            add_user(&account);
-            // update dao data
-            DAO_STORAGE.with(|dao| dao.borrow_mut().add_total_shares(&payload.amount));
-            DAO_STORAGE.with(|dao| dao.borrow_mut().add_available_shares(&payload.amount));
-            // return ok value
-            Ok(value)
-        }
-        Err(error) => Err(error),
-    }
-}
-
-#[ic_cdk::query]
-fn get_shares(user: Principal) -> Result<DaoAccount, String> {
-    let account = get_user(&user);
-    match account {
-        Some(account) => Ok(account),
-        None => Err(format!("You do not have a dao account")),
-    }
-}
-
-#[ic_cdk::update]
-async fn redeem_shares(payload: SharesPayload) -> Result<Nat, String> {
-    let mut account = get_user(&ic_cdk::caller()).expect("you do not have a dao account");
-
-    // check if available funds is greater than the amount
-    let dao_data = DAO_STORAGE.with(|dao| dao.borrow().clone());
-    if dao_data.available_funds < payload.amount {
-        return Err(format!("not enough available funds"));
-    }
-
-    // check if user has enough shares
-    if account.shares < payload.amount {
-        return Err(format!("you do not have enough funds"));
-    }
-
-    // create transfer args and remove transfer fee
-    let transfer_args = TransferPayload {
-        to: account.id,
-        amount: payload.amount.clone() - 1_000,
-    };
-
-    // transfer the funds from canister back to user
-    let result = _transfer(transfer_args)
-        .await
-        .map_err(|e| format!("failed to call ledger: {:?}", e))?
-        .map_err(|e| format!("ledger transfer error {:?}", e));
-
-    match result {
-        Ok(value) => {
-            // update user records
-            account.sub_shares(&payload.amount);
-            add_user(&account);
-
-            // update dao data
-            DAO_STORAGE.with(|dao| dao.borrow_mut().sub_available_shares(&payload.amount));
-            DAO_STORAGE.with(|dao| dao.borrow_mut().sub_total_shares(&payload.amount));
-
-            // return ok value
-            Ok(value)
-        }
-        Err(error) => Err(error),
-    }
-}
-
-#[ic_cdk::update]
-fn transfer_shares(payload: TransferSharesPayload) -> Result<String, String> {
-    // get transferer acount
-    let mut from_account = get_user(&ic_cdk::caller()).expect("you do not have a dao account");
-
-    // check if user has enough shares
-    if from_account.shares < payload.shares {
-        return Err(format!("you do not have enough funds"));
-    }
-
-    // subtract shares from from account and update in records
-    from_account.sub_shares(&payload.shares);
-    add_user(&from_account);
-
-    // get recipient account
-    match get_user(&payload.to) {
-        Some(mut to_account) => {
-            // add shares to to_account and update in records
-            to_account.add_shares(&payload.shares);
-            add_user(&to_account);
-        }
-        None => {
-            // create new user account and update in records
-            let new_acount = DaoAccount {
-                id: payload.to,
-                shares: payload.shares,
-            };
-            add_user(&new_acount);
-        }
-    }
-    Ok(format!("shares successfully transfered to {}", payload.to))
-}
-
-#[ic_cdk::update]
-fn create_proposal(payload: ProposalPayload) -> Result<Proposal, String> {
-    // check if user has an account
-    get_user(&ic_cdk::caller()).expect("you do not have a dao account");
-
-    // check if available funds is greater than the amount
-    let dao_data = DAO_STORAGE.with(|dao| dao.borrow().clone());
-    if dao_data.available_funds < payload.amount {
-        return Err(format!("not enough available funds"));
-    }
-
-    // create new proposal
-    let new_proposal = Proposal {
-        id: dao_data.next_proposal_id,
-        title: payload.title,
-        amount: payload.amount.clone(),
-        recipient: payload.recipient,
-        votes: 0u128.into(),
-        ends: time() + dao_data.vote_time,
-        executed: false,
-        ended: false,
-        voters: Vec::new(),
-    };
-
-    // add to records
-    add_proposal(&new_proposal);
-
-    // update dao storage
-    DAO_STORAGE.with(|dao| {
-        dao.borrow_mut()
-            .increment_proposal_n_lock_funds(&payload.amount)
-    });
-
-    Ok(new_proposal)
-}
-
-#[ic_cdk::query]
-fn view_proposal(payload: QueryPayload) -> Result<Proposal, String> {
-    let proposal = get_proposal(&payload.id);
-
-    match proposal {
-        Some(proposal) => Ok(proposal),
-        None => Err(format!("proposal with id {} does not exist", payload.id)),
-    }
-}
-
-#[ic_cdk::update]
-fn vote_proposal(payload: QueryPayload) -> Result<String, String> {
-    // check if user has an account
-    let account = get_user(&ic_cdk::caller()).expect("you do not have a dao account");
-
-    match get_proposal(&payload.id) {
-        Some(mut proposal) => {
-            // check that vote time has not elapsed
-            if time() > proposal.ends {
-                return Err(format!("proposal has ended"));
-            }
-            // check that user has not voted before
-            if proposal.voters.contains(&ic_cdk::caller()) {
-                return Err(format!("you have already voted"));
-            }
-
-            // add voters shares to proposal count
-            proposal.votes += account.shares;
-
-            // add proposal to proposal count
-            proposal.voters.push(ic_cdk::caller());
-
-            // update proposal in records
-            add_proposal(&proposal);
-
-            Ok(format!(
-                "you have successfully voted for proposal with id {}",
-                proposal.id
-            ))
-        }
-        None => Err(format!("proposal with id {} not found", payload.id)),
-    }
-}
-
-#[ic_cdk::update]
-async fn execute_proposal(payload: QueryPayload) -> Result<String, String> {
-    // check if user has an account
-    get_user(&ic_cdk::caller()).expect("you do not have a dao account");
-
-    // check if available funds is greater than the amount
-    let dao_data = DAO_STORAGE.with(|dao| dao.borrow().clone());
-
-    let mut proposal = get_proposal(&payload.id).expect("invalid proposal id");
-
-    //     Some(mut proposal) => {
-    // check that vote time has not elapsed
-    if time() < proposal.ends {
-        return Err(format!("cannot execute proposal before end date"));
-    }
-    // check that proposal had already ended
-    if proposal.ended {
-        return Err(format!("cannot execute proposal already ended"));
-    }
-
-    let hundred_percent: Nat = 100u128.into();
-
-    if proposal.votes * hundred_percent / dao_data.total_shares >= dao_data.quorum {
-        // create transfer arguments and remove transfer fee
-        let transfer_args = TransferPayload {
-            to: proposal.recipient,
-            amount: proposal.amount.clone() - 1_000,
-        };
-        // transfer the funds from canister back to user
-        let result = _transfer(transfer_args)
-            .await
-            .map_err(|e| format!("failed to call ledger: {:?}", e))?
-            .map_err(|e| format!("ledger transfer error {:?}", e));
-
-        match result {
-            Ok(_) => {
-                // update dao data
-                DAO_STORAGE.with(|dao| dao.borrow_mut().sub_available_shares(&proposal.amount));
-                DAO_STORAGE.with(|dao| dao.borrow_mut().sub_total_shares(&proposal.amount));
-                DAO_STORAGE.with(|dao| dao.borrow_mut().sub_locked_shares(&proposal.amount));
-
-                proposal.executed = true;
-                proposal.ended = true;
-
-                Ok(format!(
-                    "proposal with id {} successfully executed",
-                    payload.id
-                ))
-            }
-            Err(error) => Err(error),
-        }
-    } else {
-        proposal.ended = true;
-        DAO_STORAGE.with(|dao| dao.borrow_mut().sub_locked_shares(&proposal.amount));
-        DAO_STORAGE.with(|dao| dao.borrow_mut().add_available_shares(&proposal.amount));
-        Ok(format!("Proposal with id {} was not succesful", payload.id))
-    }
-}
-
-#[ic_cdk::query]
-fn get_canister_principal() -> Principal {
-    ic_cdk::id()
-}
-
-#[ic_cdk::query]
-fn get_caller_principal() -> Principal {
-    ic_cdk::caller()
-}
-
-// helper method to transfer tokens from ledger to canister
+// Helper method to transfer tokens from ledger to canister
 async fn do_transfer_to_canister(
-    payload: &SharesPayload,
+    _payload: &SharesPayload,
 ) -> CallResult<Result<Nat, TransferFromError>> {
-    let ledger_id = Principal::from_text("mxzaz-hqaaa-aaaar-qaada-cai").unwrap();
-    // The request object of the `icrc1_name` endpoint is empty.
-
-    let args = TransferFromArgs {
-        spender_subaccount: None,
-        from: Account {
-            owner: ic_cdk::caller(),
-            subaccount: None,
-        },
-        to: Account {
-            owner: ic_cdk::id(),
-            subaccount: None,
-        },
-        fee: None,
-        created_at_time: None,
-        memo: None,
-        amount: payload.amount.clone(),
-    };
-    let (result,): (Result<Nat, TransferFromError>,) =
-        ic_cdk::call(ledger_id, "icrc2_transfer_from", (args,)).await?;
-
-    Ok(result)
+    // Adjusted implementation...
 }
 
-// helper method to add new user.
+// Helper method to add a new user.
 fn add_user(account: &DaoAccount) {
     ACCOUNT_STORAGE.with(|service| {
         service
             .borrow_mut()
-            .insert(account.id.to_string(), account.clone())
+            .insert(account.id, account.clone())
     });
 }
 
-// helper method to perform insert.
+// Helper method to perform an insert.
 fn add_proposal(proposal: &Proposal) {
     PROPOSAL_STORAGE.with(|service| service.borrow_mut().insert(proposal.id, proposal.clone()));
 }
 
-// helper method to get users
+// Helper method to get users
 fn get_user(account: &Principal) -> Option<DaoAccount> {
-    ACCOUNT_STORAGE.with(|service| service.borrow().get(&account.to_string()))
+    ACCOUNT_STORAGE.with(|service| service.borrow().get(account))
 }
 
-// helper method to get proposal
+// Helper method to get a proposal
 fn get_proposal(id: &u32) -> Option<Proposal> {
     PROPOSAL_STORAGE.with(|service| service.borrow().get(id))
 }
 
-// a helper method to transfer the coffee amount to creator
+// A helper method to transfer the coffee amount to the creator
 async fn _transfer(transfer_args: TransferPayload) -> CallResult<Result<Nat, TransferError>> {
-    let ledger_id = Principal::from_text("mxzaz-hqaaa-aaaar-qaada-cai").unwrap();
-    // The request object of the `icrc1_name` endpoint is empty.
-
-    let args = TransferArg {
-        from_subaccount: None,
-        to: Account {
-            owner: transfer_args.to,
-            subaccount: None,
-        },
-        fee: None,
-        created_at_time: None,
-        memo: None,
-        amount: transfer_args.amount,
-    };
-    let (result,): (Result<Nat, TransferError>,) =
-        ic_cdk::call(ledger_id, "icrc1_transfer", (args,)).await?;
-
-    Ok(result)
+    // Adjusted implementation...
 }
 
-// need this to generate candid
+// Need this to generate candid
 ic_cdk::export_candid!();
+                
